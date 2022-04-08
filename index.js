b48122085246be496096c2e40809f5d39a968d23
cyberlengman <78582171+cyberlengman@users.noreply.github.com>
2022-04-07 22:35:32 -0600

    fixed broken explore button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const projects = document.getElementById('button2');
 const bubble = document.getElementsByTagName('li');
 
 const scrollToStack = () => {
-  const stack = document.getElementById('stack');
+  const stack = document.getElementsByClassName('skills');
   if (stack) {
     stack.scrollIntoView({ behavior: 'smooth' });
   }
@@ -14,11 +14,6 @@ const scrollToProjects = () => {
   if (projects) {
     stack.scrollIntoView({ behavior: 'smooth' });
   }
-}
-
-const distort = (bubble) => {
-  bubble.style.transform = 'scale3d(1.1, 1.1, 1.1)';
-  bubble.style.transition = 'transform 0.7s';
 }
 
 explore.addEventListener('click', scrollToStack);
